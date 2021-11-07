aa772bc27538ab1a5ab33186f59d22e56a6ab096
kaikaiy17 <kaikaibrock@gmail.com>
2021-11-06 19:05:20 -0500

    Added in test cases for simulation results, but they still require transaction that can be tested against. Skeletons of simulation functions have been added with a few being implemented. However, all implemented ones need to be reviewed

--- background/SimulationSuite.ts
+++ background/SimulationSuite.ts
@@ -1,8 +1,16 @@
+import { defaultAbiCoder, Interface } from "@ethersproject/abi";
 import { Provider } from "@ethersproject/abstract-provider";
+import { getContractAddress } from "@ethersproject/address";
+import { hexDataSlice } from "@ethersproject/bytes";
 import { Transaction } from "@ethersproject/transactions";
+import { ethers } from "ethers";
+
+// Source: https://ethereum.stackexchange.com/questions/11144/how-to-decode-input-data-from-a-transaction
+    // To decode transaction data
 
 export class SimulationSuite {
     provider: Provider;
+    erc20interface: Interface;
 
     /**
      * SimulationSuite constructor
@@ -10,6 +18,7 @@ export class SimulationSuite {
      */
     constructor(provider: Provider) {
         this.provider = provider;
+        this.erc20interface = new ethers.utils.Interface(require('../erc20abi.json'));
     }
 
     /**
@@ -21,7 +30,25 @@ export class SimulationSuite {
      * @returns true if the transaction matches the function description
      */
     async isTokenTransferToContract(t: Transaction): Promise<Boolean> {
-        return false;
+        // If destination address is not a contract address, return false
+        /** Using getCode() to determine if a contract is associated with the address
+         *  Source: https://ethereum.stackexchange.com/questions/83017/how-do-you-know-if-a-contract-is-destroyed
+         *  Source: https://docs.ethers.io/v5/api/providers/provider/
+        */
+        // Checking if transaction is a token transfer and then checking if the destination address is a contract address
+        var res = true;
+        try {
+            var input = await this.erc20interface.parseTransaction({data: t.data});
+            var dest = input.args[0];
+            var code = await this.provider.getCode(dest);
+            if (code === "0x") {
+                res = false;
+            }
+        } catch(e) {
+            res = false;
+        };
+
+        return res;
     }
 
     /**
@@ -30,7 +57,95 @@ export class SimulationSuite {
      * @param t Transaction to test
      * @returns true if the transaction matches the function description
      */
+    // TODO: Needs to be fixed. Depending on the function, it is difficult to determine which parameter is the destination address
+    // Using a defaultAbiCoder and using its decoding function requires guessing of the parameter types unless if somehow manually
+    // parse out the data and recognize the 42-character address (including 0x). However, we would also need to know the number of 
+    // parameters the function in the data takes
     async isDataSentToEOA(t: Transaction): Promise<Boolean> {
+        var res = true;
+        try {
+            var input = await this.erc20interface.parseTransaction({data: t.data});
+            var dest = input.args[0];
+            var code = await this.provider.getCode(dest);
+            if (code !== "0x") {
+                res = false;
+            }
+        } catch(e) {
+            console.log(e);
+            res = false;
+        };
+
+        return res;
+    }
+
+    /**
+     * Returns true if the transaction's gas price is not too high or too low
+     * @param t Transaction to test
+     * @returns true if the transaction matches the function description
+     */
+    async isGasPriceReasonable(t: Transaction): Promise<Boolean> {
+        var tGasPrice = t.gasPrice;
+        // Both total gas fees measured in ETH
+        var tGasTotalFees = (await tGasPrice.mul(t.gasLimit)).mul(0.000000001);
+        var estimatedTotalGasFees = (await this.provider.estimateGas(t)).mul(tGasPrice).mul(0.000000001);
+        var estimate50 = estimatedTotalGasFees.mul(.5);
+
+        if (estimatedTotalGasFees.sub(estimate50) > tGasTotalFees || estimatedTotalGasFees.add(estimate50) < tGasTotalFees) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns true if the transaction has an empty data fields
+     * @param t Transaction to test
+     * @returns true if the transaction matches the function description
+     */
+    async hasEmptyDataFields(t: Transaction): Promise<Boolean> {
+        var dest = t.to;
+        var amount = t.value;
+        var gasPrice = t.gasPrice;
+
+        if (dest === "" || amount === null || gasPrice === null) {
+            return true;
+        }
+
         return false;
     }
+
+    /**
+     * Returns true if the transaction is being sent on the right network
+     * @param t Transaction to test
+     * @returns true if the transcation matches the function description
+     */
+    async isOnRightNetwork(t: Transaction): Promise<Boolean> {
+        return true;
+    }
+
+    /**
+     * Returns true if the transaction is sent to an address it has not been sent to before
+     * @param t Transaction to test
+     * @returns true if the transaction matches the function description
+     */
+    async isNewAddress(t: Transaction): Promise<Boolean> {
+        return false;
+    }
+
+    /**
+     * Returns true if the total amount of the transaction is more than what is in the user's wallet
+     * @param t Transaction to test
+     * @returns true if the transaction matches the function description
+     */
+    async isTotalMoreThanWallet(t: Transaction): Promise<Boolean> {
+        return false;
+    }
+
+    /**
+     * Other Tests to make:
+     * 
+     * Other outrageous parameter magnitudes
+     * Will the transaction revert (runs out of gas) if it were included in the next block?
+     * Could user's priority fee be safely lowered?
+     */
 }
--- tests/SimulationResultTest.ts
+++ tests/SimulationResultTest.ts
@@ -13,33 +13,93 @@ describe("SimulationResults tests", () => {
         sr = new SimulationSuite(provider);
     });
 
+    // TODO: Fix Error: Timeout of 2000ms exceeded. For async tests and hooks, ensure "done()" is called; if returning a Promise, ensure it resolves
+        // All tests do pass when there is no timeout error
     it("Can detect direct ERC-20 token transfers to smart contracts", async () => {
         // Token transfer to contract address
         var t: Transaction = await provider.getTransaction("0x04cb2b86afadc05ee00af7b9f5d58c676372ac7549bcfebc0c07012df4fe8690");
         expect(await sr.isTokenTransferToContract(t))
             .to.be.true;
+            
         // Call to contract address; not a token transfer
         t = await provider.getTransaction("0x26c10475f0e6a73b45ee73f54743c459f428efa899d0a4c6ec72d65dc23380f1");
-        expect(sr.isTokenTransferToContract(t))
+        expect(await sr.isTokenTransferToContract(t))
             .to.be.false;
+
         // Token transfer to non-contract address
+        // TODO: Check to make sure that there is not a contract associated with this addresss
+            // Against implementation and running provider.getCode(), there is some contract associated with this address
         t = await provider.getTransaction("0x8431016caad2a1bed1d4a7b4e6b2c9d6c797ee6f3f775c694cace3ddc7d12e98");
-        expect(sr.isTokenTransferToContract(t))
+        expect(await sr.isTokenTransferToContract(t))
             .to.be.false;
     });
 
     it("Can detect data sent to non-contract addresses", async () => {
         // Data sent to non-contract address
         var t: Transaction = await provider.getTransaction("0xeb0d50c3d14cfce8f84ae86ac4cde37d4913c6d6b331bf9a61197c3e1f0f27e5");
-        expect(await sr.isTokenTransferToContract(t))
+        expect(await sr.isDataSentToEOA(t))
             .to.be.true;
         // Data sent to contract address
         t = await provider.getTransaction("0xbb13c1de1d4dd0aea8fc9e5ea22f4362c76d9a24d6ef7706e23eaae529820f70");
-        expect(await sr.isTokenTransferToContract(t))
+        expect(await sr.isDataSentToEOA(t))
             .to.be.false;
         // No data sent
         t = await provider.getTransaction("0x26c10475f0e6a73b45ee73f54743c459f428efa899d0a4c6ec72d65dc23380f1");
-        expect(await sr.isTokenTransferToContract(t))
+        expect(await sr.isDataSentToEOA(t))
             .to.be.false;
     });
+
+    // it("Can determine that the gas price is reasonable", async () => {
+    //     // Transaction's gas price is reasonable
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isGasPriceReasonable(t)).to.be.true;
+
+    //     // Transaction's gas price is too high
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isGasPriceReasonable(t)).to.be.false;
+
+    //     // Transaction's gas price is too low
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isGasPriceReasonable(t)).to.be.false;
+    // });
+
+    // it("Can detect empty data fields", async () => {
+    //     // Transaction has an _____ data field
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.hasEmptyDataFields(t)).to.be.true;
+
+    //     // Transaction has no empty data fields
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.hasEmptyDataFields(t)).to.be.false;
+    // });
+
+    // it("Can detect that the transaction is being sent on the right network", async () => {
+    //     // Transaction is sent on the right network
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isOnRightNetwork(t)).to.be.true;
+
+    //     // Transaction is sent on the wrong network
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isOnRightNetwork(t)).to.be.false;
+    // });
+
+    // it("Can detect that a transaction is being sent to a new address", async () => {
+    //     // Transaction is being sent to a new address
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isNewAddress(t)).to.be.true;
+
+    //     // Transaction is not being sent to a new address
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isNewAddress(t)).to.be.false;
+    // });
+
+    // it("Can detect that the total amount of the transaction is more than what exists in the wallet", async () => {
+    //     // Transaction costs more than what user has in wallet
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isTotalMoreThanWallet(t)).to.be.true;
+
+    //     // Transaction amount is within the wallet's capacity
+    //     var t: Transaction = await provider.getTransaction("");
+    //     expect(await sr.isTotalMoreThanWallet(t)).to.be.false;
+    // });
 });
