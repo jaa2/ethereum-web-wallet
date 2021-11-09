83d158879934b4654df01b4ddf6b600891d374f2
happythemis <42981449+happythemis@users.noreply.github.com>
2021-11-09 14:26:37 +0900

    edit small for private key and restore secret phrase

--- src/wallet_setup/ImportPrivateKey.tsx
+++ src/wallet_setup/ImportPrivateKey.tsx
@@ -1,9 +1,40 @@
-function ImportPrivateKey() {
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEthereum } from '@fortawesome/free-brands-svg-icons';
+import { faCloudUploadAlt, faKey, faPlus } from '@fortawesome/free-solid-svg-icons';
+
+
+function ImportSecretPhrase() {
   return (
-    <div>
-      ImportPrivateKey
+    <div id= "import-secret-phrase">
+      <div id="wallet-setup-header">
+        <FontAwesomeIcon className="fa-icon" icon={faEthereum} size="3x" />
+        <h1>Icon Here</h1>
+      </div>
+
+      <h1>Import Private Key</h1>
+
+      <h4>Enter your private key below</h4>
+      
+
+      
+
+
+
+      
+
+
+
     </div>
   );
 }
-  
-export default ImportPrivateKey;
+
+function UserInputPhrase() {
+  var   num1= ((document.getElementById("num1") as HTMLInputElement).value);
+  var   num2= ((document.getElementById("num2") as HTMLInputElement).value);
+  var result=parseInt(num1)+parseInt(num2);
+  console.log(result);
+}
+
+export default ImportSecretPhrase;
