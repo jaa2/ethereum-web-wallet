5e58aaa3393ca698b958ec442d7f55e24439592c
happythemis <42981449+happythemis@users.noreply.github.com>
2021-11-09 03:47:20 -0600

    base structure for privateKey

--- src/wallet_setup/ImportPrivateKey.tsx
+++ src/wallet_setup/ImportPrivateKey.tsx
@@ -1,40 +1,67 @@
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEthereum } from '@fortawesome/free-brands-svg-icons';
-import { faCloudUploadAlt, faKey, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faUnlock } from '@fortawesome/free-solid-svg-icons';
 
+import './CreatePassword.scss';
+
+function CorrectPrivateKey() {
+  console.log("Correct Private Key LogIn!");
+}
 
 function ImportSecretPhrase() {
+  const [privateKeyMatchState, setPrivateKeyMatchState]: [string, (matchState: string) => void] = React.useState<string>("empty");
+
+  const [privateKey, setPrivateKey]: [string, (privateKey: string) => void] = React.useState<string>("");
+  const handlePrivateKey = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPrivateKey(event.target.value);
+  }
+
+  const [confirmPrivateKey, setConfirmPrivateKey]: [string, (confirmPrivateKey: string) => void] = React.useState<string>("");
+  const handleConfirmPrivateKey = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPrivateKey(event.target.value);
+  }
+
+
+  let privateKeyMatchElements: JSX.Element = ( <div /> );
+  // handle additional state where the user's password doesn't meet requirements
+  if (privateKeyMatchState === "match") {
+    privateKeyMatchElements = (
+      <div id="correct-private-key">
+        <p id="correct-private-key-info" className="private-key-info" >Successful Login!</p>
+        <Link id="correct-private-key-continue-link" className="link hoverable" to="/Home" onClick={CorrectPrivateKey} >
+          <h4>Continue</h4>
+        </Link>
+      </div>
+    );
+  } else {
+    privateKeyMatchElements = (
+    <div id="wrong-private-key">
+      <p id="wrong-private-key-info" className="private-key-info">Wrong Private Key!</p>
+    </div>
+    );
+  } 
+
   return (
-    <div id= "import-secret-phrase">
-      <div id="wallet-setup-header">
-        <FontAwesomeIcon className="fa-icon" icon={faEthereum} size="3x" />
+    <div id= "import-private-key">
+      <div>
+       
         <h1>Icon Here</h1>
       </div>
 
       <h1>Import Private Key</h1>
 
       <h4>Enter your private key below</h4>
-      
+      <div id="enter-private-key-entry" className="field-entry">
+          <input id="private-key-input" type="text" name="privateKey" onChange={handlePrivateKey} />
+      </div>
 
       
-
-
-
-      
-
-
-
     </div>
+      
   );
 }
 
-function UserInputPhrase() {
-  var   num1= ((document.getElementById("num1") as HTMLInputElement).value);
-  var   num2= ((document.getElementById("num2") as HTMLInputElement).value);
-  var result=parseInt(num1)+parseInt(num2);
-  console.log(result);
-}
 
 export default ImportSecretPhrase;
