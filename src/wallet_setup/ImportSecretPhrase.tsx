dde712aba300573832f8aac357fbda90cbf088ac
happythemis <42981449+happythemis@users.noreply.github.com>
2021-11-08 22:51:34 -0600

    test small changes

--- src/wallet_setup/ImportSecretPhrase.tsx
+++ src/wallet_setup/ImportSecretPhrase.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEthereum } from '@fortawesome/free-brands-svg-icons';
+import { faCloudUploadAlt, faKey, faPlus } from '@fortawesome/free-solid-svg-icons';
+
+
 function ImportSecretPhrase() {
   return (
-    <div>
-      ImportSecretPhrase
+    <div id= "import-secret-phrase">
+      <div id="wallet-setup-header">
+        <FontAwesomeIcon className="fa-icon" icon={faEthereum} size="3x" />
+        <h1>Icon Here</h1>
+      </div>
+
+      <h1>Import Private Key</h1>
     </div>
   );
 }
