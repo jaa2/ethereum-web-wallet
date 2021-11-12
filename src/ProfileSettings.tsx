7c3cad76834b98ae84dbd8af0fcdb3ca66fdd619
happythemis <42981449+happythemis@users.noreply.github.com>
2021-11-12 09:37:58 -0600

    settings update

--- src/ProfileSettings.tsx
+++ src/ProfileSettings.tsx
@@ -7,28 +7,34 @@ import { faCloudUploadAlt, faKey, faPlus, faUsers, faUserEdit, faUserCircle, faE
 import './ProfileSettings.scss'
 
 function ProfileSettings() {
-
-
-
   return (
     <div id="profile-settings">
 
       <div id="profile-left-section">
-        <div id="settings-nav-button">
-          <div>
+        <div id="settings-nav">
+          <Link id="wallet-setup-import-key" className="link hoverable" to="/Home">
+            <FontAwesomeIcon className="fa-icon" icon={faHome} size="1x" />
+            <h4>Home</h4>
+          </Link>
+          <Link id="wallet-setup-import-key" className="link hoverable" to="/SignIn">
+            <FontAwesomeIcon className="fa-icon" icon={faLock} size="1x" />
+            <h4>SignIn</h4>
+          </Link>
+
+          {/* <div id="settings-nav-button">
           <Link to="/Home">
             <FontAwesomeIcon className="fa-icon" icon={faHome} size="1x" />
             <button>Home</button>
           </Link>
 
           </div>
-          <div>
+          <div id="settings-nav-button">
             <Link to="/SignIn">
             <FontAwesomeIcon className="fa-icon" icon={faLock} size="1x" />
             <button>Lock</button>
             </Link>
 
-          </div>
+          </div> */}
         </div>
         
         <div id="profile-picture-edit">
@@ -46,11 +52,30 @@ function ProfileSettings() {
 
       <div id="profile-right-section">
         <h3>Profile & Settings</h3>
-        <h5>Delay Time</h5>
+        <div id="delay-selection">
+          <div id="delay-selection-content">
+            <h5 >Delay Time</h5>
+          </div>
+          <div id="delay-selection-content">
+            <select id="to-address-input" name="to-address">
+                  <option>5 sec</option>
+                  <option>10 sec</option>
+                  <option>20 sec</option>
+                  <option>30 sec</option>
+            </select>
+          </div>
+          
+        </div>
         <div id="dark-mode">
-          <h5>Dark Mode</h5>
-          <input type="checkbox"/>
-          <span id="slider"></span>
+          <div id="dark-mode-content">
+            <h5>Dark Mode</h5>
+          </div>
+          <div id="dark-mode-content">
+            <input type="checkbox"/>
+          </div>
+          
+          
+          {/* <span id="slider"></span> */}
         </div>
         
 
