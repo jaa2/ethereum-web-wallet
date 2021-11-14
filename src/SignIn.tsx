a55405a136ae5b25e1a9125b532b990f328cb9fd
chbkull@gmail.com <chkull2@illinois.edu>
2021-11-14 15:05:12 -0600

    Bootstrap styled SignIn, added eslint rule

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -18,16 +18,14 @@ function SignIn() {
 
   return (
     <div id="sign-in">
-      <div id="sign-in-header">
-        <FontAwesomeIcon className="fa-icon" icon={faLock} size="3x" />
-        <h1>Ethereum Web Wallet Login</h1>
-      </div>
-      <div id="sign-in-entry" className="field-entry">
-        <h5 id="sign-in-password-label">Password</h5>
-        <input id="sign-in-password-input" type="password" name="password" onChange={handlePassword} />
+      <FontAwesomeIcon className="fa-icon" icon={faLock} size="3x" />
+      <h1>Ethereum Web Wallet Login</h1>
+      <div id="sign-in-entry" className="form-group mb-3">
+        <label className="form-label" htmlFor="sign-in-password-input">Password</label>
+        <input type="password" id="sign-in-password-input" className="form-control" onChange={handlePassword} placeholder="Password" />
       </div>
       <Link to="/Home">
-        <button id="sign-in-login-button" className="bottom-button" type="button" onClick={() => Authenticate(password)}>Login</button>
+        <button type="button" id="sign-in-login-button" className="btn btn-outline-primary" onClick={() => Authenticate(password)}>Login</button>
       </Link>
     </div>
   );
