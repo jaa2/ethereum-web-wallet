3cccf1985d684c3e1501f105377e2b22a517d4ab
jaa2 <43010335+jaa2@users.noreply.github.com>
2021-11-15 12:28:13 -0600

    Fix state handling for TokenTransferBox

--- src/transaction/SimulationResults.tsx
+++ src/transaction/SimulationResults.tsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGasPump, faFire, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { faEthereum } from '@fortawesome/free-brands-svg-icons';
+import { TransactionRequest } from '@ethersproject/abstract-provider';
 
 import './SimulationResults.scss';
 import { TokenTransferBox } from './TokenTransferBox';
 
 function SimulationResults() {
+  const tx: TransactionRequest = {
+    from: '0xd67e28a63cfa5381d3d346d905e2f1a6471bde11',
+    to: '0xe592427a0aece92de3edee1f18e0157c05861564',
+    data: '0x414bf389000000000000000000000000b4fbf271143f4fbf7b91a5ded31805e42b2208d60000000000000000000000001f9840a85d5af5bf1d1762f925bdaddc4201f9840000000000000000000000000000000000000000000000000000000000000bb8000000000000000000000000d67e28a63cfa5381d3d346d905e2f1a6471bde11000000000000000000000000000000000000000000000000000000016191490900000000000000000000000000000000000000000000000000038d7ea4c680000000000000000000000000000000000000000000000000000002fa215d28153b0000000000000000000000000000000000000000000000000000000000000000',
+    value: '0x38d7ea4c68000',
+  };
   return (
     <div id="simulation-results">
 
@@ -72,7 +79,7 @@ function SimulationResults() {
       </div>
 
       <h1>Token Transfers</h1>
-      <TokenTransferBox />
+      <TokenTransferBox tx={tx} />
       <h4>
         0x51092...094ef to
         <b>Timmy Turner</b>
--- src/transaction/TokenTransferBox.tsx
+++ src/transaction/TokenTransferBox.tsx
@@ -4,24 +4,24 @@ import { ethers } from 'ethers';
 import React from 'react';
 import { getTransferLogs, TokenTransfer } from './LogTracer';
 
+interface TokenTransferProps {
+  tx: TransactionRequest;
+}
+
 export interface TokenTransferBox {
   state: {
     tokenTransfers: Array<TokenTransfer>;
     tx: TransactionRequest;
-  }
+  },
+  props: TokenTransferProps;
 }
 
-export class TokenTransferBox extends React.Component {
-  constructor(props: any) {
+export class TokenTransferBox extends React.Component<TokenTransferProps> {
+  constructor(props: TokenTransferProps) {
     super(props);
     this.state = {
       tokenTransfers: Array<TokenTransfer>(),
-      tx: {
-        from: '0xd67e28a63cfa5381d3d346d905e2f1a6471bde11',
-        to: '0xe592427a0aece92de3edee1f18e0157c05861564',
-        data: '0x414bf389000000000000000000000000b4fbf271143f4fbf7b91a5ded31805e42b2208d60000000000000000000000001f9840a85d5af5bf1d1762f925bdaddc4201f9840000000000000000000000000000000000000000000000000000000000000bb8000000000000000000000000d67e28a63cfa5381d3d346d905e2f1a6471bde11000000000000000000000000000000000000000000000000000000016191490900000000000000000000000000000000000000000000000000038d7ea4c680000000000000000000000000000000000000000000000000000002fa215d28153b0000000000000000000000000000000000000000000000000000000000000000',
-        value: '0x38d7ea4c68000',
-      },
+      tx: props.tx,
     };
     this.load();
   }
