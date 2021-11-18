77ecb87dadf3c2aefcf9dd50f61a605728c43b55
happythemis <42981449+happythemis@users.noreply.github.com>
2021-11-17 22:28:52 -0600

    edit left section and delete button

--- src/ProfileSettings.tsx
+++ src/ProfileSettings.tsx
@@ -21,6 +21,9 @@ function ProfileSettings() {
           <div id="profile-picture-edit">
             <FontAwesomeIcon className="fa-icon" icon={faUserCircle} size="7x" />
             <FontAwesomeIcon className="fa-icon" icon={faEdit} size="1x" />
+            {/* <div className="form-group">
+              <input className="form-control" type="file" id="formFile" />
+            </div> */}
           </div>
           <div id="profile-name-edit">
             <text>Name</text>
@@ -30,33 +33,21 @@ function ProfileSettings() {
         </div>
         <div id="profile-right-section">
           <div id="delay-selection">
-            <div id="delay-selection-content">
-              {/* <h5>Delay Time</h5> */}
+            {/* <form> */}
+            <fieldset>
               <legend className="mt-1">Delay Time</legend>
-            </div>
-            {/* <div id="delay-selection-content">
-              <select id="to-address-input" name="to-address">
-                <option>5 sec</option>
-                <option>10 sec</option>
-                <option>20 sec</option>
-                <option>30 sec</option>
-              </select>
-            </div> */}
-            <form>
-              <fieldset>
-                <div className="form-group">
-                  <select className="form-select" id="exampleSelect1">
-                    <option>5 sec</option>
-                    <option>10 sec</option>
-                    <option>20 sec</option>
-                    <option>30 sec</option>
-                  </select>
-                </div>
-              </fieldset>
-            </form>
+              <div className="form-group">
+                <select className="form-select" id="exampleSelect1">
+                  <option>5 sec</option>
+                  <option>10 sec</option>
+                  <option>20 sec</option>
+                  <option>30 sec</option>
+                </select>
+              </div>
+            </fieldset>
+            {/* </form> */}
           </div>
           <div id="dark-mode">
-            {/* <h5>Dark Mode</h5> */}
             <fieldset>
               <legend className="mt-1">Dark Mode</legend>
               <div className="form-check form-switch">
@@ -71,89 +62,6 @@ function ProfileSettings() {
       </div>
     </div>
   );
-  //   <div id="profile-settings">
-
-  // <div id="profile-left-section">
-  //   <div id="settings-nav">
-  //     <Link id="wallet-setup-import-key" className="link hoverable" to="/Home">
-  //       <FontAwesomeIcon className="fa-icon" icon={faHome} size="1x" />
-  //       <h6>Home</h6>
-  //     </Link>
-  //     <Link id="wallet-setup-import-key" className="link hoverable" to="/SignIn">
-  //       <FontAwesomeIcon className="fa-icon" icon={faLock} size="1x" />
-  //       <h6>SignIn</h6>
-  //     </Link>
-  //   </div>
-  //   <div id="profile-picture-edit">
-  //     <FontAwesomeIcon className="fa-icon" icon={faUserCircle} size="7x" />
-  //     <FontAwesomeIcon className="fa-icon" icon={faEdit} size="1x" />
-  //   </div>
-
-  // //   <div id="profile-name-edit">
-  // //     <text>Name</text>
-  // //     <FontAwesomeIcon className="fa-icon" icon={faEdit} size="1x" />
-  // //   </div>
-  // //   <button type="button">0x51912454541...</button>
-  // // </div>
-
-  //     <div id="profile-right-section">
-  //       <h3>Profile & Settings</h3>
-  //       <div id="delay-selection">
-  //         <div id="delay-selection-content">
-  //           <h5>Delay Time</h5>
-  //         </div>
-  //         <div id="delay-selection-content">
-  //           <select id="to-address-input" name="to-address">
-  //             <option>5 sec</option>
-  //             <option>10 sec</option>
-  //             <option>20 sec</option>
-  //             <option>30 sec</option>
-  //           </select>
-  //         </div>
-  //       </div>
-  //       <div id="dark-mode">
-  //         <div id="dark-mode-content">
-  //           <h5>Dark Mode</h5>
-  //         </div>
-  //         <div id="dark-mode-content">
-  //           <input type="checkbox" />
-  //         </div>
-  //         {/* <span id="slider"></span> */}
-  //       </div>
-  //       <button type="button">Delete Account</button>
-  //     </div>
-  //   </div>
-  // );
 }
 
 export default ProfileSettings;
-// import { Link } from 'react-router-dom';
-
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCloudUploadAlt, faHome } from '@fortawesome/free-solid-svg-icons';
-
-// import './ProfileSettings.scss';
-
-// function ProfileSettings() {
-//   return (
-//     <div className="container">
-//       <Link className="back-icon" to="/WalletSetup">
-//         <FontAwesomeIcon className="fa-icon" icon={faHome} size="2x" />
-//       </Link>
-//       <div className="container">
-//         <FontAwesomeIcon className="fa-icon" icon={faCloudUploadAlt} size="4x" />
-//         <h1>Import Secret Recovery Phrase</h1>
-//         <p>Enter your 12-word secret recovery phrase below.</p>
-//         <div className="form-group">
-//           <textarea className="form-control is-valid" id="secret-phrase" rows={4} />
-//           <div className="valid-feedback">Success. This is a valid wallet account!</div>
-//         </div>
-//         <Link to="/CreatePassword">
-//           <button type="button" className="btn btn-success">Continue</button>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ProfileSettings;
