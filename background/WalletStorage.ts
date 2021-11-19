abc56eb0bc1b1c5a90188f157df7332ea5ad7622
jaa2 <43010335+jaa2@users.noreply.github.com>
2021-11-19 00:53:11 -0600

    Add delete function to WalletState

--- background/WalletStorage.ts
+++ background/WalletStorage.ts
@@ -9,4 +9,5 @@ export const storageVersion: Number = 1;
 export interface WalletStorage {
   get(keys?: string | string[] | Record<string, any>): Promise<Record<string, any>>;
   set(items: Record<string, any>): Promise<void>;
+  remove(keys: string | string[]): Promise<void>;
 }
