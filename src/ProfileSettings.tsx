a687cb2a3259afb56f3b389f9af898df9df52acc
happythemis <42981449+happythemis@users.noreply.github.com>
2021-11-18 23:35:40 -0600

    fix center align and back button styling

--- src/ProfileSettings.tsx
+++ src/ProfileSettings.tsx
@@ -1,43 +1,83 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faUserCircle, faEdit, faHome,
+  faUserCircle, faEdit, faExclamationTriangle, faCogs, faArrowCircleLeft,
 } from '@fortawesome/free-solid-svg-icons';
 import './ProfileSettings.scss';
+import Modal from 'react-bootstrap/Modal';
 import AddressBox from './common/AddressBox';
+
+const DangerConfim = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const showModal = () => {
+    setIsOpen(true);
+  };
+
+  const hideModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <div className="button-container">
+        <button type="button" className="btn btn-outline-danger" onClick={showModal}>Delete Account</button>
+        <Modal show={isOpen} onHide={hideModal}>
+          <Modal.Header>
+            <div id="max-tx-fee">
+              <h3>
+                <FontAwesomeIcon className="fa-icon" icon={faExclamationTriangle} size="2x" color="#489aca" />
+                {' '}
+                Confirm Delete Account
+              </h3>
+            </div>
+          </Modal.Header>
+          <Modal.Body>
+            If you delete account, you can not restore this account.
+            Are you sure to delete your account?
+          </Modal.Body>
+          <Modal.Footer>
+            <button type="button" className="btn btn-secondary" onClick={hideModal}>Cancel</button>
+            <button type="button" className="btn btn-danger" onClick={hideModal}>Delete</button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </>
+  );
+};
 
 function ProfileSettings() {
   return (
-    <div id="profile-settings">
-      <div className="container">
-        <Link className="back-icon" to="/WalletSetup">
-          <FontAwesomeIcon className="fa-icon" icon={faHome} size="2x" />
-        </Link>
-        <h1>Settings</h1>
-      </div>
-      <div className="content-container">
-        <div className="profile-left-section">
-          <div className="profile-picture-edit">
-            <div className="pic-section">
-              <FontAwesomeIcon className="fa-icon" icon={faUserCircle} size="9x" />
+    <div id="profile-settings" className="container">
+      <Link className="back-icon" to="/Home">
+        <FontAwesomeIcon className="fa-icon" icon={faArrowCircleLeft} size="2x" />
+      </Link>
+      <div id="content" className="container">
+        <FontAwesomeIcon className="fa-icon" icon={faCogs} size="4x" />
+        <h1 className="text-center">Settings</h1>
+        <div id="content-container" className="container">
+          <div className="profile-left-section">
+            <div className="profile-picture-edit">
+              <div className="pic-section">
+                <FontAwesomeIcon className="fa-icon" icon={faUserCircle} size="9x" />
+              </div>
+              <div className="edit-pic-section">
+                <FontAwesomeIcon className="fa-icon" icon={faEdit} size="1x" />
+              </div>
             </div>
-            <div className="edit-pic-section">
-              <FontAwesomeIcon className="fa-icon" icon={faEdit} size="1x" />
+            <div className="profile-name-edit">
+              <div>
+                <p>Name</p>
+              </div>
+              <div>
+                <FontAwesomeIcon className="fa-icon" icon={faEdit} size="1x" />
+              </div>
             </div>
+            {/* TODO: get address from state */}
+            <AddressBox address="0x510928a823b" />
           </div>
-          <div className="profile-name-edit">
-            <div>
-              <p>Name</p>
-            </div>
-            <div>
-              <FontAwesomeIcon className="fa-icon" icon={faEdit} size="1x" />
-            </div>
-          </div>
-          {/* TODO: get address from state */}
-          <AddressBox address="0x510928a823b" />
-        </div>
-        <div id="profile-right-section">
-          <div id="delay-selection">
+          <div id="profile-right-section">
             <fieldset>
               <legend className="mt-1">Delay Time</legend>
               <div className="form-group">
@@ -49,20 +89,19 @@ function ProfileSettings() {
                 </select>
               </div>
             </fieldset>
-          </div>
-          <div id="dark-mode">
-            <fieldset>
-              <legend className="mt-1">Dark Mode</legend>
-              <div className="form-check form-switch">
-                <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
-              </div>
-            </fieldset>
+            {/* </div> */}
+            <div id="dark-mode">
+              <fieldset>
+                <legend className="mt-1">Dark Mode</legend>
+                <div className="form-check form-switch">
+                  <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                </div>
+              </fieldset>
+            </div>
           </div>
         </div>
       </div>
-      <div className="button-container">
-        <button type="button" className="btn btn-outline-danger">Delete Account</button>
-      </div>
+      <DangerConfim />
     </div>
   );
 }
